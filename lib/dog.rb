17e3f7c59348f0373ea655d03c879186c331b68d
Marie (Cho) Hughes <kchohee0630@gmail.com>
2020-01-31 14:16:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,64 +7,20 @@ class Dog
     @id = id
   end
   def self.create_table
-    sql=<<-SQL
-    CREATE TABLE IF NOT EXISTS dogs(
-    name TEXT
-    breed INTEGER 
-    )
-    SQL
-    DB[:conn].execute(sql)
   end
   def self.drop_table
-    sql=<<-SQL 
-    DROP TABLE dogs
-    SQL
-    DB[:conn].execute(SQL)
   end
   def save
-    if self.id
-      self.update
-    else
-      sql = <<-SQL
-        INSERT INTO dogs (name, breed)
-        VALUES (?, ?)
-      SQL
-      DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
-    end
   end
   def self.create(name:,breed:)
-    dog = Dog.new(name, breed)
-    dog.save
-    dog
   end
   def self.new_from_db(row)
-    dog = Dog.new(name: row[1], breed: row[2], id: row[0])
-    dog
   end
   def self.find_by_id(id)
-    sql = "SELECT * FROM dogs WHERE id = ?"
-    result = DB[:conn].execute(sql, id)[0]
-    Dog.new(result[0], result[1], result[2])
   end
   def self.find_or_create_by(name:,breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-    if !dog.empty?
-      dog_data = dog[0]
-      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
-    else
-      dog = self.create(name: name, breed: breed)
-    end
-    dog
   end
   def self.find_by_name(name)
-    sql = <<-SQL
-    SELECT *
-    FROM dogs
-    WHERE name = ?
-    SQL
-    dog = DB[:conn].execute(sql, name)[0]
-    Dog.new_from_db(dog)
   end
   def update
   end
