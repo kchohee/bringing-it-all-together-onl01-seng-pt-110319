d0403b0bda2861a8018704325ff438ef1d02ad10
Marie (Cho) Hughes <kchohee0630@gmail.com>
2020-01-31 13:58:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,14 +7,6 @@ class Dog
     @id = id
   end
   def self.create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS dogs (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      breed TEXT
-    )
-    SQL
-    DB[:conn].execute(sql)
   end
   def self.drop_table
   end
