447fedfd39a6b85f2cb4997603d0e5f6b52a77bb
Marie (Cho) Hughes <kchohee0630@gmail.com>
2020-01-31 14:09:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,6 +46,15 @@ class Dog
     Dog.new(result[0], result[1], result[2])
   end
   def self.find_or_create_by(name:,breed:)
+    def self.find_or_create_by(name:, album:)
+    song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
+    if !song.empty?
+      song_data = song[0]
+      song = Song.new(song_data[0], song_data[1], song_data[2])
+    else
+      song = self.create(name: name, breed: )
+    end
+    song
   end
   def self.find_by_name(name)
   end
